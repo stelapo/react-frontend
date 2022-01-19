b5f6a7572b65751167d90bbe280c2bbb2c2fbf33
Lapo Pancani <stelapo@gmail.com>
2022-01-19 12:40:53 +0100

    Aggiunto routing

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-        <div className="container">
-          <ListEmployeeComponent />
-        </div>
-      <FooterComponent />
+      <Router>
+        <HeaderComponent/>
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<ListEmployeeComponent/>}></Route>
+              <Route path='/employees' element={<ListEmployeeComponent/>}></Route>
+            </Routes>
+          </div>
+        <FooterComponent />
+      </Router>
     </div>
   );
 }
